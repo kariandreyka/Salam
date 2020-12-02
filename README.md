9db030cc2ee4ebd1a0fb855084d65828e7ede3f9
Demchuk Andrey <kariandreyka@gmail.com>
2020-12-02 06:57:42 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+#Salam
+
+*Salam* - TypeScript-powered online video room, wich client and server, based on React and Express.JS.
+
+##Documentation
+
+Check latest documentation in the [docs](https://github.com/kariandreyka/Salam/videochat_docs) folder.
+
+Read Project [SRS](https://github.com/kariandreyka/Salam/videochat_docs/README.md) to check requirements.
+
+Read UML Diagrams to check project structure.
+
+##System Modules
+
+##Client
+Salams web client was written on javascript, using next js framework for ReactJS and WebRTC. Key features & libraries:
+    -React Router for web navigation 
+    -Redax for data management
+##Server
+Salam server was written on node js with TypeScript, expres js and soket.io. Key features & libraries:
+    -Socket.io for signalling
+    -Express js for API
+
+##Start the project
+
+```bash
+git clone https://github.com/kariandreyka/Salam.git
+cd videochat_client 
+npm run dev
+```
+
+
+
+
+
+
+
+
+
---
+++ videochat_server/src/server.ts
@@ -0,0 +1,71 @@
+import express , { Application } from "express";
+import { Server as SocketIOServer } from "socket.io";
+import { createServer, Server as HTTPServer } from "http";
+
+export class Server {
+    private httpServer: HTTPServer;
+    private app: Application;
+    private io: SocketIOServer;
+    private activeSockets: string[] = [];
+
+    private readonly DEFAULT_PORT = 5000;
+
+    constructor() {
+        this.app = express();
+        this.httpServer = createServer(this.app);
+        this.io = new SocketIOServer(this.httpServer);
+
+        this.handleRoutes();
+        this.handleSocketConnection();
+    }
+
+    private handleRoutes(): void{
+        this.app.get('/', (req, res) => {
+            res.send('<h1>Hello world</h1>');
+        });
+    }
+
+    private handleSocketConnection(): void{
+        this.io.on('connection', socket =>{
+            const existingSocket = this.activeSockets.find(
+                existingSocket => existingSocket === socket.id
+            );
+            if(!existingSocket){
+                this.activeSockets.push(socket.id);
+            }
+
+            socket.broadcast.emit('update-user-list', {
+                users: [socket.id]
+            });
+
+            socket.on('disconnect', () =>{
+                this.activeSockets = this.activeSockets.filter(
+                    existingSocket => existingSocket != socket.id
+                )
+                socket.broadcast.emit('remove-user', {
+                    socketId: socket.id
+                });
+            });
+
+            socket.on("call-user", (data: { to: any; offer: any; }) => {
+                socket.to(data.to).emit("call-made", {
+                    offer: data.offer,
+                    socket: socket.id
+                });
+            });
+
+            socket.on('make-answer', (data: { to: any; answer: any; }) =>{
+                socket.to(data.to).emit('answer-made',  {
+                    socket: socket.id,
+                    answer: data.answer
+                });
+            });
+        });
+    }
+
+    public listen(callback: (port: number) => void): void {  
+        this.httpServer.listen(this.DEFAULT_PORT, () =>{
+            callback(this.DEFAULT_PORT)
+        });
+    }
+}
